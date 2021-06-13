02b432ca62794ceb6acca353e8d616a25c1c83a3
tholv <tholv2000@gmail.com>
2021-06-13 17:45:04 +0700

    update connect remote

--- dao/dbConnect.php
+++ dao/dbConnect.php
@@ -1,8 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbName = "staffmanagement";
+    $servername = "remotemysql.com";
+    $username = "5QMueXiug5";
+    $password = "kXv6fSysfJ";
+    $dbName = "5QMueXiug5";
     $conn = new mysqli($servername, $username, $password, $dbName);
 
     if ($conn -> connect_error) {
